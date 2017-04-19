ab469fff89930a28c73144243d8d30a078bbece9
Hisashi Kamezawa <hkame6926@gmail.com>
2017-04-19 19:02:34 +0900

    add symbolic links

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gemrc ~/.gemrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.pryrc ~/.pryrc
